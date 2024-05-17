5b2ae91dc81769225d8c3d2334409592b0820cf1
Mohamad Hijazi <moehijazi>
2024-05-17 10:10:27 +0300

    deployed

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
                 if (id.includes('node_modules')) {
                     return id.toString().split('node_modules/')[1].split('/')[0].toString();
                 }
-                if (id.indexOf("react") !== -1) { return; }
+                
             }
         }
     }
